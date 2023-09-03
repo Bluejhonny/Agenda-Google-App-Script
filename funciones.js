d12d6289d4c5e22ec7a11e57304cd3068c8beba4
bluejhonny777@gmail.com <bluejhonny777@gmail.com>
2023-09-03 19:10:34 -0400

    v2

--- funciones.js
+++ funciones.js
@@ -5,3 +5,10 @@ function doGet() {
 function obtenerDatosHtml(nombre) {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos(){
+    let hoja= SpreadsheetApp.openById('1Fnk9Le-rKhWlMXQSRpINSTiWK5s42IyB8xFLhPwkk2o').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+
+}
